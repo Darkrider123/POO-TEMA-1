e6620581638351db532b41694da9ebacd9325799
Darkrider123 <48128755+Darkrider123@users.noreply.github.com>
2019-03-22 02:47:05 +0200

    Update graf.cpp

--- graf.cpp
+++ graf.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include "graf.h"
 using namespace std;
+
+graf graf::operator =(const graf &ob)
+{
+     int i,j;
+    if (n<=0&&ob.n>0)
+    {
+
+    n=ob.n;
+     a=new int* [n+1];
+     for (i=0;i<n+1;i++)
+        a[i]=new int [n+1];
+
+        for (i=0;i<n+1;i++)
+            for (j=0;j<n+1;j++)
+             a[i][j]=ob.a[i][j];
+             return *this;
+    }
+    if (ob.n<=0)
+    {
+        n=0;
+        return *this;
+    }
+
+
+    for (i=0;i<n+1;i++)
+    delete[] a[i];
+     delete[] a;
+
+      n=ob.n;
+     a=new int* [n+1];
+     for (i=0;i<n+1;i++)
+        a[i]=new int [n+1];
+
+        for (i=0;i<n+1;i++)
+            for (j=0;j<n+1;j++)
+             a[i][j]=ob.a[i][j];
+             return *this;
+}
 
 graf graf::operator + (const graf &ob1)
  {
       int i,j;
 
-    graf z;
 
+
+    graf z(ob1.n);
     if (ob1.n!=n) return z;
+    if (ob1.n<=0) return z;
 
-    z.n=n;
      for (i=1;i<=z.n;i++)
         for (j=1;j<=z.n;j++)
         {
@@ -25,17 +65,63 @@ graf graf::operator + (const graf &ob1)
 
 graf::graf ()
  {
-    int i,j;
-    for (i=0;i<=49;i++)
-      for (j=0;j<=49;j++)
-        a[i][j]=0;
         n=0;
+ }
+
+graf::graf(const graf &ob)
+{  if (ob.n==0)
+{
+    n=0;
+}
+else{
+
+
+    int i,j;
+    n=ob.n;
+     a=new int* [n+1];
+     for (i=0;i<n+1;i++)
+        a[i]=new int [n+1];
+
+        for (i=0;i<n+1;i++)
+            for (j=0;j<n+1;j++)
+             a[i][j]=ob.a[i][j];
+}
+}
+
+graf::graf(int c)
+{int i,j;
+if (c<=0)
+{
+    n=0;
+
+}
+else {
+    n=c;
+     a=new int* [n+1];
+     for (i=0;i<n+1;i++)
+        a[i]=new int [n+1];
+
+        for (i=0;i<n+1;i++)
+            for (j=0;j<n+1;j++)
+             a[i][j]=0;
+}
+}
+
+ graf::~graf()
+ {
+    int i;
+    if (n<=0) return;
+    for (i=0;i<n+1;i++)
+    delete[] a[i];
+
+     delete[] a;
  }
 
 
 
 ostream & operator <<(ostream & out,const graf &ob)
 {int i,j;
+if (ob.n<=0) return out;
     for (i=1;i<=ob.n;i++)
     {
         for (j=1;j<=ob.n;j++)
@@ -47,17 +133,21 @@ ostream & operator <<(ostream & out,const graf &ob)
 
 istream & operator >>(istream &in, graf &ob)
 {int x;
-
+int i,j;
 cout<<"Introdu numarul de noduri: ";
      in>>ob.n;
      cout<<"\n";
+     if (ob.n<=0) return in;
 
+     ob.a=new int* [ob.n+1];
+     for (i=0;i<ob.n+1;i++)
+        ob.a[i]=new int [ob.n+1];
 
      cout<<"\n Introdu un graf sub forma de matrice de adiacenta de marime "<<ob.n<<"x"<<ob.n<<"\n\n";
-     int i,j;
 
-      for (i=0;i<50;i++)
-                    for (j=0;j<50;j++)
+
+      for (i=0;i<ob.n+1;i++)
+                    for (j=0;j<ob.n+1;j++)
                         ob.a[i][j]=0;
 
     for (i=1;i<=ob.n;i++)
@@ -68,8 +158,8 @@ cout<<"Introdu numarul de noduri: ";
                    {
                        cout<<"\n Graful nu are costuri, deci valorile introduse trebuie sa fie cuprinse intre 0 si 1\n\n";
 
-                    for (i=0;i<50;i++)
-                    for (j=0;j<50;j++)
+                    for (i=0;i<ob.n+1;i++)
+                    for (j=0;j<ob.n+1;j++)
                         ob.a[i][j]=0;
                         i=1;
                         j=1;
@@ -83,7 +173,7 @@ cout<<"Introdu numarul de noduri: ";
 }
 
 
-int recursie (int a[50][50],int &n,int &cautat,int curent,int &c)
+int recursie (int **a,int &n,int &cautat,int curent,int &c)
 {int i;
      if (c==0) return 0;
      c--;
@@ -97,10 +187,10 @@ int recursie (int a[50][50],int &n,int &cautat,int curent,int &c)
 
 }
 
-graf::DeterminareDrumuri()
+void graf::DeterminareDrumuri()
 {
 
-   int b[50][50],i,j,c;
+   int b[n][n],i,j,c;
 
    for (i=1;i<=n;i++)
     for (j=1;j<=n;j++)
@@ -119,9 +209,9 @@ graf::DeterminareDrumuri()
 }
 
 
-graf::ComponenteConexe()
+void graf::ComponenteConexe()
 {
-       int i,j,c,v[60],vizitate[60]={0},ok,k;
+       int i,j,c,v[600],vizitate[600]={0},ok;
 
 
 
@@ -167,7 +257,7 @@ graf::ComponenteConexe()
 
 }
 
-graf::ETareConex()
+void graf::ETareConex()
 {
 
 int i,j,c,ok=1;
@@ -185,8 +275,6 @@ int i,j,c,ok=1;
 }
 
 /* Incercare cu vectori de vizitati
-
-
 int recursie (int a[50][50],int n,int cautat,int curent,int c[])
 {int i;
      if (c[curent]!=0) return 0;
@@ -196,29 +284,20 @@ int recursie (int a[50][50],int n,int cautat,int curent,int c[])
             for (i=1;i<=n;i++)
                 if (a[curent][i]==1)
                if(recursie(a,n,cautat,i,c)==1) return 1;
-
           return 0;
-
 }
-
 graf::DeterminareDrumuri()
 {
-
    int b[50][50],i,j,c[60]={0};
-
    for (i=1;i<=n;i++)
     for (j=1;j<=n;j++)
       b[i][j]=a[i][j];
-
     for (i=1;i<=n;i++)
         for (j=1;j<=n;j++)
           if (recursie(a,n,j,i,c)==1)b[i][j]=1;
-
      for (i=1;i<=n;i++)
     for (j=1;j<=n;j++)
       a[i][j]=b[i][j];
-
       cout<<*this;
 }
-
 */
